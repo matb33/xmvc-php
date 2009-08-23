fa950441ea1857e9a188d9d1d5ef1c9659cec90b
matb33@gmail.com <matb33@gmail.com@669c4392-8544-11de-aa20-d7387d5ed10e>
2009-08-23 00:06:34 +0000

    * Renamed base to named.  Base was not an appropriate name for the named-templates app method

---
+++ named/app/libraries/language.php
@@ -0,0 +1,68 @@
+<?php
+
+function GetLang()
+{
+	$language = NULL;
+	
+	$languages = new Model( "xml" );
+	$languages->xml->Load( "languages" );
+	
+	$defaultLanguage = $languages->xml->xPath->query( "//lang:languages/lang:config/lang:default" )->item( 0 )->nodeValue;
+	
+	foreach( $languages->xml->xPath->query( "//lang:languages/lang:language-list/lang:language" ) as $node )
+	{
+		$id			 = $node->getAttribute( "lang:id" );
+		$hostMatch	 = $languages->xml->xPath->query( "lang:host-match", $node )->item( 0 )->nodeValue;
+	
+		$data[ $id ] = array( 
+			"host-match" => $hostMatch
+		);
+	}
+	
+	if( !in_array( $defaultLanguage, array_keys( $data ) ) )
+	{
+		$language = $languages->xml->xPath->query( "//lang:languages/lang:language-list/lang:language[ 1 ]/@lang:id" )->item( 0 )->nodeValue;
+	}
+	
+	foreach( $data as $key => $info )
+	{
+		if( strpos( $_SERVER[ "HTTP_HOST" ], $info[ "host-match" ] ) !== false )
+		{
+			$language = $key;
+			break;
+		}
+	}
+	
+	if( is_null( $language ) )
+	{
+		if( isset( $_GET[ "lang" ] ) )
+		{
+			if( in_array( $_GET[ "lang" ], array_keys( $data ) ) )
+			{
+				$language = $_GET[ "lang" ];	
+			}
+		}
+	}
+	
+	if( is_null( $language ) )
+	{
+		if( isset( $_SESSION[ "lang" ] ) )
+		{
+			if( in_array( $_SESSION[ "lang" ], array_keys( $data ) ) )
+			{
+				$language = $_SESSION[ "lang" ];	
+			}
+		}
+	}
+	
+	if( is_null( $language ) )
+	{
+		$language = $defaultLanguage;
+	}
+	
+	$_SESSION[ "lang" ] = $language;
+	
+	return( $language );
+}
+
+?>
