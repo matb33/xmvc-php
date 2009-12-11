fe3e0d1edba04497291f48473693ba350d4b65d2
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2009-12-11 19:50:30 +0000

    * Added XSLT Profiling option * Added registerPHPFunctions in XSLT processor

--- trunk/sys/driver.php
+++ trunk/sys/driver.php
@@ -7,7 +7,7 @@ class ModelDriver extends DOMDocument
 
 	public function __construct()
 	{
-		parent::__construct();
+		parent::__construct( "1.0", "UTF-8" );
 
 		$this->preserveWhiteSpace		= true;
 		$this->formatOutput				= true;
