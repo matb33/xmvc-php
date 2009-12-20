a7b5009dda96d39620676007439eb03910bdc518
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2009-12-20 02:43:22 +0000

    * Changed config loading priority order * Renamed xslt-compatible-browsers to xslt * Changed system load controller to accept namespaces (using ::) in the URL * Swapped order that config arrays are merged

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ NamespaceMap::Register( "/^xMVC::Sys::(.*)$/", "./sys/%f/%1" );
 NamespaceMap::Register( "/^xMVC::App::(.*)$/", "./app/%f/%1" );
 NamespaceMap::Register( "/^Module::(.*?)::(.*)$/", "./mod/%1/%f/%2" );
 
+Config::Load( "./sys" );
 Config::Load( "./app" );
 Config::Load( "./mod/*" );
-Config::Load( "./sys" );
 
 Core::Load();
 
