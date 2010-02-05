f3e267ab6bfae2cf3982ca510d38dbe7ff9b7eb7
schnare.d <schnare.d@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-02-05 21:55:20 +0000

    * removed the leftover "string" type hints * fixed some errors in the event example

--- trunk/sys/libraries/eventdispatcher.php
+++ trunk/sys/libraries/eventdispatcher.php
@@ -14,12 +14,12 @@ namespace xMVC\Sys;
      * Adds an event listener for the specified event type to this event dispatcher.
      * Calling this multiple times with the same arguments will only result in one listener being added.
      */
-    public function addEventListener(string $eventType, Delegate $delegate);
+    public function addEventListener($eventType, Delegate $delegate);
     
     /**
      * Removes an event listener for the specified event type from this event dispatcher.
      */
-    public function removeEventListener(string $eventType, Delegate $delegate);    
+    public function removeEventListener($eventType, Delegate $delegate);    
     
     /**
      * Removes all event listeners listening to this event dispatcher.
