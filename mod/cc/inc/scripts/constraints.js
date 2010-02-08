07f4816a88fac01e6484f6a2c0eabd57bf11b448
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-02-08 20:57:21 +0000

    * Added missing inc folder for cc module

---
+++ mod/cc/inc/scripts/constraints.js
@@ -0,0 +1,231 @@
+var Constraints = new function()
+{
+	this.ajaxURL = null;
+	this.allowOnNextSubmitEvent = false;
+
+	this.Initialize = function( ajaxURL, form )
+	{
+		this.ajaxURL = ajaxURL;
+		this.form = form;	// not active -- investigate why this.form.find() barely ever works
+
+		this.BindFieldEvents();
+	};
+
+	this.BindFieldEvents = function()
+	{
+		$( "form input[ type='text' ], input[ type='password' ], textarea, select" ).change( function()
+		{
+			Constraints.AskServer( $( this ) );
+		});
+
+		$( "form input[ type='radio' ], input[ type='checkbox' ]" ).click( function()
+		{
+			Constraints.AskServer( $( this ) );
+		});
+
+		$( "form" ).submit( function()
+		{
+			if( Constraints.allowOnNextSubmitEvent )
+			{
+				return( true );
+			}
+			else
+			{
+				Constraints.AskServer( $( this ) );
+				return( false );
+			}
+		});
+	};
+
+	this.AskServer = function( field )
+	{
+		ConstraintVisuals.OnAskServer( this.GetFieldCollection( field ) );
+
+		$.ajax( {
+			url: this.ajaxURL,
+			type: "POST",
+			async: true,
+			data: this.GetParameters( this.GetUniquelyNamedFieldCollection( field ) ),
+			dataType: "json",
+			success: function( data, textStatus ) { Constraints.OnResponseFromServer( data, textStatus, field ) }
+		});
+	};
+
+	this.OnResponseFromServer = function( data, textStatus, eventField )
+	{
+		var receivedProperResponse = ( textStatus == "success" );
+
+		if( receivedProperResponse )
+		{
+			var valid = false;
+			var collectionValid = true;
+
+			for( var i = 0; i < data.length; i++ )
+			{
+				var field = $( "form *[ name='" + this.EscapeName( data[ i ].name ) + "' ], form *[ name='" + this.EscapeName( data[ i ].name ) + "\\[\\]' ]" );
+
+				if( "results" in data[ i ] )
+				{
+					valid = true;
+
+					for( var j = 0; j < data[ i ].results.length; j++ )
+					{
+						var result = data[ i ].results[ j ];
+						valid = valid && result.success;
+						collectionValid = collectionValid && valid;
+					}
+
+					ConstraintVisuals.OnFieldConstraintResult( field, valid );
+				}
+			}
+
+			if( eventField.is( "form" ) )
+			{
+				this.allowOnNextSubmitEvent = collectionValid;
+
+				if( collectionValid )
+				{
+					$( "form" ).submit();
+				}
+			}
+		}
+	};
+
+	this.GetParameters = function( fieldCollection )
+	{
+		var fieldList = {};
+
+		fieldCollection.each( function()
+		{
+			fieldList[ Constraints.StripBrackets( this.name ) ] = Constraints.GetValue( $( this ) );
+
+			Constraints.GetDependencyFieldCollection( this.name ).each( function()
+			{
+				fieldList[ Constraints.StripBrackets( this.name ) ] = Constraints.GetValue( $( this ) );
+			});
+		});
+
+		return( fieldList );
+	};
+
+	this.GetUniquelyNamedFieldCollection = function( field )
+	{
+		var fieldCollection = this.GetFieldCollection( field );
+		var lookup = [];
+
+		var uniqueFieldCollection = fieldCollection.filter( function()
+		{
+			var name = $( this ).attr( "name" );
+
+			if( lookup.indexOf( name ) != -1 )
+			{
+				return( false );
+			}
+			else
+			{
+				lookup.push( name );
+				return( true );
+			}
+		});
+
+		return( uniqueFieldCollection );
+	};
+
+	this.GetFieldCollection = function( field )
+	{
+		if( field.is( ":submit" ) || field.is( "form" ) )
+		{
+			var fieldCollection = $( "form input[ type != 'hidden' ], form textarea, form select" );
+		}
+		else
+		{
+			var fieldCollection = $( "form *[ name='" + Constraints.EscapeName( field.attr( "name" ) ) + "' ]" );
+		}
+
+		return( fieldCollection );
+	};
+
+	this.GetDependencyFieldCollection = function( name )
+	{
+		var dependencyFieldCollection = $( "input[ name='" + Constraints.EscapeName( this.StripBrackets( name ) + "--dependency[]" ) + "' ]" ).map( function()
+		{
+			return( $( "form *[ name='" + Constraints.EscapeName( this.value ) + "' ]" ).get() );
+		});
+
+		return( dependencyFieldCollection );
+	};
+
+	this.GetValue = function( field )
+	{
+		if( field.is( ":radio" ) || field.is( ":checkbox" ) )
+		{
+			var checkedFields = $( "form input[ name='" + Constraints.EscapeName( field.attr( "name" ) ) + "' ]:checked" );
+
+			if( checkedFields.length > 0 )
+			{
+				var val = checkedFields.map( function() { return( $( this ).val() ); }).get();
+			}
+			else
+			{
+				var val = "NULL";
+			}
+		}
+		else if( field.is( "select[ multiple='true' ]" ) )
+		{
+			var val = field.val();
+
+			if( val == null || val == undefined )
+			{
+				val = "NULL";
+			}
+		}
+		else
+		{
+			var val = field.val();
+		}
+
+		if( val == null || val == undefined )
+		{
+			val = "";
+		}
+
+		return( val );
+	};
+
+	this.EscapeName = function( name )
+	{
+		return( name.replace( "[", "\\[" ).replace( "]", "\\]" ) );
+	};
+
+	this.StripBrackets = function( name )
+	{
+		return( name.replace( "[", "" ).replace( "]", "" ).replace( "\\[", "" ).replace( "\\]", "" ) );
+	}
+}
+
+var ConstraintVisuals = new function()
+{
+	this.OnAskServer = function( fieldCollection )
+	{
+		fieldCollection.each( function()
+		{
+			$( this ).closest( "label" ).addClass( "constraint-loading" );
+		});
+	};
+
+	this.OnFieldConstraintResult = function( field, valid )
+	{
+		var closestLabel = field.closest( "label" );
+
+		closestLabel.removeClass( "constraint-loading" );
+
+		if( valid )
+		{
+			closestLabel.removeClass( "constraint-valid" );
+		}
+		else
+		{
+			closestLabel.removeClass( "constraint-invalid" );
+		}
+	};
+}
