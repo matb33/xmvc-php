2f5edd917a505f7b1e435d2c12e770b07934d108
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-02-08 19:07:35 +0000

    * Housecleaning in preparation for update to CC module, and relocating inc folders

--- trunk/mod/favicon/controllers/favicon.php
+++ trunk/mod/favicon/controllers/favicon.php
@@ -12,19 +12,19 @@ class Favicon
 	public function Ico()
 	{
 		header( "Content-type: image/vnd.microsoft.icon" );
-		echo( file_get_contents( "./inc/images/favicon.ico" ) );
+		echo( file_get_contents( "./app/inc/images/favicon.ico" ) );
 	}
 
 	public function Gif()
 	{
 		header( "Content-type: image/gif" );
-		echo( file_get_contents( "./inc/images/favicon.gif" ) );
+		echo( file_get_contents( "./app/inc/images/favicon.gif" ) );
 	}
 
 	public function Png()
 	{
 		header( "Content-type: image/png" );
-		echo( file_get_contents( "./inc/images/favicon.png" ) );
+		echo( file_get_contents( "./app/inc/images/favicon.png" ) );
 	}
 }
 
