a96612959d34540bfeb0c5f3ed12edf414a26dcb
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-02-17 01:46:07 +0000

    * Added basic translate script creator * Cleaned up unused namespace declarations

--- mod/cc/config/routes.php
+++ mod/cc/config/routes.php
@@ -3,5 +3,6 @@
 $routes[ "/^\/robots\.txt$/" ] = "Module\\CC\\robotstxt";
 $routes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "Module\\CC\\sitemapxml/view/%1";
 $routes[ "/^\/ccms(.*)/" ] = "Module\\CC\\ccms-admin%1";
+$routes[ "/^\/--translate(.*)/" ] = "Module\\CC\\translate/lang%1";
 
 ?>
--- mod/cc/controllers/robotstxt.php
+++ mod/cc/controllers/robotstxt.php
@@ -2,7 +2,6 @@
 
 namespace Module\CC;
 
-use xMVC\Sys\Loader;
 use xMVC\Sys\OutputHeaders;
 use xMVC\Sys\Config;
 
