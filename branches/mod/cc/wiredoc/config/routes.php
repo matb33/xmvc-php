8ca0808094ea1d829d4094b7f697b6c328c94293
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-04 20:15:15 +0000

    * Added low priority routes for wiredoc processor

--- branches/mod/cc/wiredoc/config/routes.php
+++ branches/mod/cc/wiredoc/config/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-$routes[ "/^\/robots\.txt$/" ] = "Module\\CC\\robotstxt";
-$routes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "Module\\CC\\sitemapxml/view/%1";
-$routes[ "/^\/ccms(.*)/" ] = "Module\\CC\\ccms-admin%1";
-$routes[ "/^\/--translate(.*)/" ] = "Module\\CC\\translate/lang%1";
+$lowPriorityRoutes[ "/^\/robots\.txt$/" ] = "Module\\CC\\robotstxt";
+$lowPriorityRoutes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "Module\\CC\\sitemapxml/view/%1";
+$lowPriorityRoutes[ "/^\/ccms(.*)/" ] = "Module\\CC\\ccms-admin%1";
+$lowPriorityRoutes[ "/^\/--translate(.*)/" ] = "Module\\CC\\translate/lang%1";
+
+$lowPriorityRoutes[ "/^\/(.*)$/" ] = "Module\\CC\\processor/page/%1";
 
 ?>
