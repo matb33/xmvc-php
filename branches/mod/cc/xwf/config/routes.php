7eef7edd8f6d06c78f38eee2b6c3e68c3f49963c
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-11 19:10:25 +0000

    * Built constraints into CC module so that a very small amount of code is needed to use them

--- branches/mod/cc/xwf/config/routes.php
+++ branches/mod/cc/xwf/config/routes.php
@@ -3,6 +3,7 @@
 $routes[ "/^\/robots\.txt$/" ] = "Module\\CC\\robotstxt";
 $routes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "Module\\CC\\sitemapxml/view/%1";
 $routes[ "/^\/ccms(.*)/" ] = "Module\\CC\\ccms-admin%1";
+$routes[ "/^\/constraints(.*)/" ] = "Module\\CC\\constraintprocessor/index%1";
 $routes[ "/^\/--translate(.*)/" ] = "Module\\CC\\translate/lang%1";
 
 ?>
