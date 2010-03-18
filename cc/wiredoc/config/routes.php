c6b0d5391baa4a4cd06d613ba1b56edc54300244
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-18 15:30:25 +0000

    * Committing work to wiredoc

--- cc/wiredoc/config/routes.php
+++ cc/wiredoc/config/routes.php
@@ -2,7 +2,6 @@
 
 $lowPriorityRoutes[ "/^\/robots\.txt$/" ] = "Module\\CC\\robotstxt";
 $lowPriorityRoutes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "Module\\CC\\sitemapxml/view/%1";
-$lowPriorityRoutes[ "/^\/ccms(.*)/" ] = "Module\\CC\\ccms-admin%1";
 $lowPriorityRoutes[ "/^\/--translate(.*)/" ] = "Module\\CC\\translate/lang%1";
 
 $lowPriorityRoutes[ "/^\/(.*)$/" ] = "Module\\CC\\processor/page/%1";
