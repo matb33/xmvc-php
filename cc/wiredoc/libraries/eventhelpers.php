7d2493b0f951d8317ab7b3e4da03f6fb9aa1859c
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-29 16:35:24 +0000

    * Added param1, param2 etc to CC module's events * Added xmlmodeldriver caching helper for events

--- cc/wiredoc/libraries/eventhelpers.php
+++ cc/wiredoc/libraries/eventhelpers.php
@@ -4,6 +4,8 @@ namespace Module\CC;
 
 use xMVC\Sys\Events\Event;
 use xMVC\Sys\Delegate;
+use xMVC\Sys\XMLModelDriver;
+use xMVC\Sys\FileSystem;
 
 class EventHelpers
 {
@@ -19,6 +21,30 @@ class EventHelpers
 	protected function Talk( $sourceModel, Event &$event )
 	{
 		CC::GetEventPump()->dispatchEvent( new Event( "onComponentBuildComplete", array( "sourceModel" => $sourceModel, "data" => $event->arguments ) ) );
+	}
+
+	protected function GetCachedXMLModelDriver( $modelName, Event &$event, $updateCacheEveryXMinutes = 1440 )
+	{
+		$cacheFolder = "app/inc/cache/" .  $event->type . "/";
+		$cacheFile = md5( $modelName . floor( time() / ( $updateCacheEveryXMinutes * 60 ) ) ) . ".xml";
+
+		if( file_exists( $cacheFolder . $cacheFile ) )
+		{
+			$model = new XMLModelDriver( $cacheFolder . $cacheFile );
+		}
+		else
+		{
+			$model = new XMLModelDriver( $modelName );
+
+			FileSystem::CreateFolderStructure( $cacheFolder );
+
+			if( FileSystem::TestPermissions( $cacheFolder, FileSystem::FS_PERM_WRITE ) )
+			{
+				file_put_contents( $cacheFolder . $cacheFile, $model->saveXML() );
+			}
+		}
+
+		return( $model );
 	}
 }
 
