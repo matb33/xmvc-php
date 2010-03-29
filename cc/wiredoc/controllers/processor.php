7e19496ef475a52abc69002e5b6891aabbf816d1
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-29 17:45:27 +0000

    * Restructured CC processor code

--- cc/wiredoc/controllers/processor.php
+++ cc/wiredoc/controllers/processor.php
@@ -5,6 +5,7 @@ namespace Module\CC;
 use xMVC\Sys\Core;
 use xMVC\Sys\Routing;
 use xMVC\Sys\Loader;
+use xMVC\Sys\Config;
 use xMVC\Sys\ErrorHandler;
 use xMVC\Sys\XMLModelDriver;
 use xMVC\Sys\StringsModelDriver;
@@ -57,41 +58,82 @@ class Processor extends \xMVC\App\Website
 	{
 		$instance = $linkData[ "name" ];
 		$component = $linkData[ "component" ];
-		$viewName = $linkData[ "view" ];
+		$viewName = $this->FallbackViewNameIfNecessary( $linkData[ "view" ] );
 
-		if( strlen( trim( $viewName ) ) == 0 )
-		{
-			$viewName = __NAMESPACE__ . "\\xhtml1-strict";
-		}
-
-		$model = new XMLModelDriver( Core::namespaceApp . "instances/" . $component . "/" . $instance );
 		$view = new View( $viewName );
-		CC::InjectReferences( $model );
-		$view->PushModel( $model );
 
-		if( XMLModelDriver::Exists( Core::namespaceApp . "instances/" . $component . "/xliff/" . $instance . "." . $this->lang, "xliff" ) )
-		{
-			$xliffModel = new XMLModelDriver( Core::namespaceApp . "instances/" . $component . "/xliff/" . $instance . "." . $this->lang . ".xliff" );
-			$view->PushModel( $xliffModel );
-		}
-
-		$stringData = new StringsModelDriver();
-		$stringData->Add( "component", $component );
-		$stringData->Add( "instance", $instance );
-		$stringData->Add( "instance-file", $instance . "." . Loader::modelExtension );
-		$stringData->Add( "view-name", $viewName );
-		$view->PushModel( $stringData );
-
-		foreach( $this->additionalModels as $additionalModel )
-		{
-			$view->PushModel( $additionalModel );
-		}
+		$this->PushInstance( $view, $component, $instance );
+		$this->PushXLIFF( $view, $component, $instance );
+		$this->PushStringData( $view, $component, $instance, $viewName );
+		$this->PushAdditionalModels( $view );
 
 		CC::InjectLinkNextToPageName( $view );
 		CC::InjectLinkNextToLangSwap( $view );
 		CC::InjectLang( $view, $this->lang );
 
 		$view->RenderAsHTML();
+	}
+
+	private function FallbackViewNameIfNecessary( $viewName )
+	{
+		if( strlen( trim( $viewName ) ) == 0 )
+		{
+			$viewName = __NAMESPACE__ . "\\xhtml1-strict";
+		}
+
+		return( $viewName );
+	}
+
+	private function PushInstance( &$view, $component, $instance )
+	{
+		$model = new XMLModelDriver( Core::namespaceApp . "instances/" . $component . "/" . $instance );
+		CC::InjectReferences( $model );
+		$view->PushModel( $model );
+	}
+
+	private function PushXLIFF( &$view, $component, $instance )
+	{
+		if( XMLModelDriver::Exists( Core::namespaceApp . "instances/" . $component . "/xliff/" . $instance . "." . $this->lang, "xliff" ) )
+		{
+			$xliffModel = new XMLModelDriver( Core::namespaceApp . "instances/" . $component . "/xliff/" . $instance . "." . $this->lang . ".xliff" );
+			$view->PushModel( $xliffModel );
+		}
+	}
+
+	private function PushStringData( &$view, $component, $instance, $viewName )
+	{
+		$basePath = Routing::URIProtocol() . "://" . $_SERVER[ "HTTP_HOST" ];
+		$link = $basePath . Routing::URI();
+
+		$stringData = new StringsModelDriver();
+		$stringData->Add( "component", $component );
+		$stringData->Add( "instance", $instance );
+		$stringData->Add( "instance-file", $instance . "." . Loader::modelExtension );
+		$stringData->Add( "view-name", $viewName );
+		$stringData->Add( "base-path", $basePath );
+		$stringData->Add( "http-host", $_SERVER[ "HTTP_HOST" ] );
+		$stringData->Add( "uri", Routing::URI() );
+		$stringData->Add( "link", $link );
+		$stringData->Add( "link-urlencoded", urlencode( $link ) );
+
+		if( isset( Config::$data[ "isProduction" ] ) && Config::$data[ "isProduction" ] )
+		{
+			$stringData->Add( "cache-buster", md5( $_SERVER[ "HTTP_HOST" ] ) );
+		}
+		else
+		{
+			$stringData->Add( "cache-buster", md5( date( "Y-m-d H:i:s" ) . rand( 0, 9999 ) ) );
+		}
+
+		$view->PushModel( $stringData );
+	}
+
+	private function PushAdditionalModels( &$view )
+	{
+		foreach( $this->additionalModels as $additionalModel )
+		{
+			$view->PushModel( $additionalModel );
+		}
 	}
 }
 
