476203c868f05d57607f37fc1b70aa6e6eb8e72f
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-31 16:54:35 +0000

    * Refactoring of image controller in CC module

--- branches/mod/cc/wiredoc/controllers/image.php
+++ branches/mod/cc/wiredoc/controllers/image.php
@@ -26,37 +26,7 @@ class Image
 
 		if( $this->VerifyResizeParameters( $width, $height, $imageFile ) )
 		{
-			list( $fullSizeWidth, $fullSizeHeight, $mimeType, $lastModified, $basename, $filename, $extension ) = ImageProcessor::GetImageData( $imageFile );
-
-			$cacheid = $width . "x" . $height . "-" . $fullSizeWidth . "x" . $fullSizeHeight . "-" . $imageFile . "-" . $lastModified;
-			$cacheFile = Config::$data[ "imageCacheFilePattern" ];
-			$cacheFile = str_replace( "#basename#", $basename, $cacheFile );
-			$cacheFile = str_replace( "#filename#", $filename, $cacheFile );
-			$cacheFile = str_replace( "#hash#", md5( $cacheid ), $cacheFile );
-			$cacheFile = str_replace( "#extension#", $extension, $cacheFile );
-			$cacheFolder = dirname( $cacheFile ) . "/";
-
-			if( file_exists( $cacheFile ) && !$force )
-			{
-				$image = $cacheFile;
-			}
-			else
-			{
-				$image = ImageProcessor::Resize( $width, $height, $imageFile );
-
-				FileSystem::CreateFolderStructure( $cacheFolder );
-
-				if( FileSystem::TestPermissions( $cacheFolder, FileSystem::FS_PERM_WRITE ) )
-				{
-					ImageProcessor::WriteImage( $image, $mimeType, $cacheFile );
-				}
-				else
-				{
-					trigger_error( "Write permissions are needed on " . $cacheFolder . " in order to use the image caching feature.", E_USER_NOTICE );
-				}
-			}
-
-			ImageProcessor::OutputImage( $image, $mimeType );
+			$this->ResizeImage( $width, $height, $imageFile, $force );
 		}
 		else
 		{
@@ -71,6 +41,41 @@ class Image
 		$imageFileTest = file_exists( $imageFile );
 
 		return( $widthTest && $heightTest && $imageFileTest );
+	}
+
+	private function ResizeImage( $width, $height, $imageFile, $force )
+	{
+		list( $fullSizeWidth, $fullSizeHeight, $mimeType, $lastModified, $basename, $filename, $extension ) = ImageProcessor::GetImageData( $imageFile );
+
+		$cacheid = $width . "x" . $height . "-" . $fullSizeWidth . "x" . $fullSizeHeight . "-" . $imageFile . "-" . $lastModified;
+		$cacheFile = Config::$data[ "imageCacheFilePattern" ];
+		$cacheFile = str_replace( "#basename#", $basename, $cacheFile );
+		$cacheFile = str_replace( "#filename#", $filename, $cacheFile );
+		$cacheFile = str_replace( "#hash#", md5( $cacheid ), $cacheFile );
+		$cacheFile = str_replace( "#extension#", $extension, $cacheFile );
+		$cacheFolder = dirname( $cacheFile ) . "/";
+
+		if( file_exists( $cacheFile ) && !$force )
+		{
+			$image = $cacheFile;
+		}
+		else
+		{
+			$image = ImageProcessor::Resize( $width, $height, $imageFile );
+
+			FileSystem::CreateFolderStructure( $cacheFolder );
+
+			if( FileSystem::TestPermissions( $cacheFolder, FileSystem::FS_PERM_WRITE ) )
+			{
+				ImageProcessor::WriteImage( $image, $mimeType, $cacheFile );
+			}
+			else
+			{
+				trigger_error( "Write permissions are needed on " . $cacheFolder . " in order to use the image caching feature.", E_USER_NOTICE );
+			}
+		}
+
+		ImageProcessor::OutputImage( $image, $mimeType );
 	}
 }
 
