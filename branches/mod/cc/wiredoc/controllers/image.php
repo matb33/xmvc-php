5244a49b21ef2e615cb315b393ca02ea68106350
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-31 16:46:37 +0000

    * Added image processing controller and library, with resizing capabilities to start

---
+++ branches/mod/cc/wiredoc/controllers/image.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace xMVC\Mod\CC;
+
+use xMVC\Sys\Config;
+use xMVC\Sys\FileSystem;
+
+class Image
+{
+	public function Resize()
+	{
+		$args = func_get_args();
+
+		$width = array_shift( $args );
+		$height = array_shift( $args );
+		$force = false;
+
+		if( $args[ count( $args ) - 1 ] == "force" )
+		{
+			$force = true;
+			array_pop( $args );
+		}
+
+		$imagePath = implode( "/", $args );
+		$imageFile = str_replace( "#image#", $imagePath, Config::$data[ "imageProcessorFolderPattern" ] );
+
+		if( $this->VerifyResizeParameters( $width, $height, $imageFile ) )
+		{
+			list( $fullSizeWidth, $fullSizeHeight, $mimeType, $lastModified, $basename, $filename, $extension ) = ImageProcessor::GetImageData( $imageFile );
+
+			$cacheid = $width . "x" . $height . "-" . $fullSizeWidth . "x" . $fullSizeHeight . "-" . $imageFile . "-" . $lastModified;
+			$cacheFile = Config::$data[ "imageCacheFilePattern" ];
+			$cacheFile = str_replace( "#basename#", $basename, $cacheFile );
+			$cacheFile = str_replace( "#filename#", $filename, $cacheFile );
+			$cacheFile = str_replace( "#hash#", md5( $cacheid ), $cacheFile );
+			$cacheFile = str_replace( "#extension#", $extension, $cacheFile );
+			$cacheFolder = dirname( $cacheFile ) . "/";
+
+			if( file_exists( $cacheFile ) && !$force )
+			{
+				$image = $cacheFile;
+			}
+			else
+			{
+				$image = ImageProcessor::Resize( $width, $height, $imageFile );
+
+				FileSystem::CreateFolderStructure( $cacheFolder );
+
+				if( FileSystem::TestPermissions( $cacheFolder, FileSystem::FS_PERM_WRITE ) )
+				{
+					ImageProcessor::WriteImage( $image, $mimeType, $cacheFile );
+				}
+				else
+				{
+					trigger_error( "Write permissions are needed on " . $cacheFolder . " in order to use the image caching feature.", E_USER_NOTICE );
+				}
+			}
+
+			ImageProcessor::OutputImage( $image, $mimeType );
+		}
+		else
+		{
+			trigger_error( "Incorrect parameters or invalid image file.", E_USER_ERROR );
+		}
+	}
+
+	private function VerifyResizeParameters( $width, $height, $imageFile )
+	{
+		$widthTest = ( ( int )$width > 0 || $width == "auto" );
+		$heightTest = ( ( int )$height > 0 || $height == "auto" );
+		$imageFileTest = file_exists( $imageFile );
+
+		return( $widthTest && $heightTest && $imageFileTest );
+	}
+}
+
+?>
---
+++ branches/mod/cc/wiredoc/libraries/imageprocessor.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace xMVC\Mod\CC;
+
+class ImageProcessor
+{
+	public static function Resize( $width, $height, $imageFile )
+	{
+		list( $fullSizeWidth, $fullSizeHeight, $mimeType, $lastModified, $b, $f, $e ) = self::GetImageData( $imageFile );
+		list( $newWidth, $newHeight ) = self::DetermineNewWidthAndHeight( $width, $height, $fullSizeWidth, $fullSizeHeight );
+
+		switch( $mimeType )
+		{
+			case "image/jpeg":
+				$fullSizeImage = imagecreatefromjpeg( $imageFile );
+			break;
+			case "image/gif":
+				$fullSizeImage = imagecreatefromgif( $imageFile );
+			break;
+			case "image/png":
+				$fullSizeImage = imagecreatefrompng( $imageFile );
+			break;
+			default:
+				return( false );
+		}
+
+		$resizedImage = imagecreatetruecolor( $newWidth, $newHeight );
+		$whiteColor = imagecolorallocate( $resizedImage, 255, 255, 255 );
+
+		imagefilledrectangle( $resizedImage, 0, 0, $newWidth, $newHeight, $whiteColor );
+		imagecopyresampled( $resizedImage, $fullSizeImage, 0, 0, 0, 0, $newWidth, $newHeight, $fullSizeWidth, $fullSizeHeight );
+
+		return( $resizedImage );
+	}
+
+	public static function GetImageData( $imageFile )
+	{
+		$imageSizeData = getimagesize( $imageFile );
+		$imageFileInfo = pathinfo( $imageFile );
+
+		return(
+			array(
+				( int )$imageSizeData[ 0 ],
+				( int )$imageSizeData[ 1 ],
+				$imageSizeData[ "mime" ],
+				filemtime( $imageFile ),
+				$imageFileInfo[ "basename" ],
+				$imageFileInfo[ "filename" ],
+				$imageFileInfo[ "extension" ]
+			)
+		);
+	}
+
+	public static function DetermineNewWidthAndHeight( $requestedWidth, $requestedHeight, $currentWidth, $currentHeight )
+	{
+		if( $requestedWidth != "auto" && $requestedHeight != "auto" )
+		{
+			$newWidth = $requestedWidth;
+			$newHeight = $requestedHeight;
+		}
+		else
+		{
+			if( $requestedWidth == "auto" && $requestedHeight != "auto" )
+			{
+				$newWidth = round( $requestedHeight / $currentHeight * $currentWidth );
+				$newHeight = $requestedHeight;
+			}
+			elseif( $requestedWidth != "auto" && $requestedHeight == "auto" )
+			{
+				$newWidth = $requestedWidth;
+				$newHeight = round( $requestedWidth / $currentWidth * $currentHeight );
+			}
+			else
+			{
+				$newWidth = $currentWidth;
+				$newHeight = $currentHeight;
+			}
+		}
+
+		return( array( ( int )$newWidth, ( int )$newHeight ) );
+	}
+
+	public static function OutputImage( $image, $mimeType )
+	{
+		header( "Content-Type: " . $mimeType );
+
+		if( is_resource( $image ) )
+		{
+			switch( $mimeType )
+			{
+				case "image/jpeg":
+					return( imagejpeg( $image ) );
+				break;
+				case "image/gif":
+					return( imagegif( $image ) );
+				break;
+				case "image/png":
+					return( imagepng( $image ) );
+				break;
+				default:
+					return( false );
+			}
+		}
+		else
+		{
+			readfile( $image );
+		}
+	}
+
+	public static function WriteImage( $imageData, $mimeType, $filename )
+	{
+		switch( $mimeType )
+		{
+			case "image/jpeg":
+				return( imagejpeg( $imageData, $filename ) );
+			break;
+			case "image/gif":
+				return( imagegif( $imageData, $filename ) );
+			break;
+			case "image/png":
+				return( imagepng( $imageData, $filename ) );
+			break;
+			default:
+				return( false );
+		}
+	}
+}
+
+?>
