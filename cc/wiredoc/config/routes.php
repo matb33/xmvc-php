2b366f8a8cb2c966ce1478448715319134176c4f
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-03-31 16:46:37 +0000

    * Added image processing controller and library, with resizing capabilities to start

--- cc/wiredoc/config/routes.php
+++ cc/wiredoc/config/routes.php
@@ -1,7 +1,8 @@
 <?php
 
 $lowPriorityRoutes[ "/^\/robots\.txt$/" ] = "xMVC\\Mod\\CC\\robotstxt";
-$lowPriorityRoutes[ "/^\/sitemap-([a-z]+)\.xml$/" ] = "xMVC\\Mod\\CC\\sitemapxml/view/%1";
+$lowPriorityRoutes[ "/^\/sitemap-([A-Za-z-]+)\.xml$/" ] = "xMVC\\Mod\\CC\\sitemapxml/view/%1";
+$lowPriorityRoutes[ "/^\/image\/(.+)\/(.*)$/" ] = "xMVC\\Mod\\CC\\image/%1/%2";
 $lowPriorityRoutes[ "/^\/--translate(.*)/" ] = "xMVC\\Mod\\CC\\translate/lang%1";
 
 $lowPriorityRoutes[ "/^\/(.*)$/" ] = "xMVC\\Mod\\CC\\processor/page/%1";
