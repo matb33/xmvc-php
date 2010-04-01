6dfb841cbafd6a4c11cf88a96aac8041d81c6b1f
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-04-01 23:37:24 +0000

    * Added Utils mod

---
+++ trunk/mod/utils/libraries/StringUtils.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace xMVC\Mod\Utils;
+
+use xMVC\Sys\Loader;
+use xMVC\Mod\Language\Language;
+
+class StringUtils
+{
+	public static function ReplaceTokensInPattern( $pattern, $tokenValuePairs, $tokenDelimiter = "#" )
+	{
+		$builtInTokenValuePairs = array();
+		$builtInTokenValuePairs[ "controllerFolder" ] = Loader::controllerFolder;
+		$builtInTokenValuePairs[ "modelFolder" ] = Loader::modelFolder;
+		$builtInTokenValuePairs[ "viewFolder" ] = Loader::viewFolder;
+		$builtInTokenValuePairs[ "driverFolder" ] = Loader::driverFolder;
+		$builtInTokenValuePairs[ "libraryFolder" ] = Loader::libraryFolder;
+		$builtInTokenValuePairs[ "configFolder" ] = Loader::configFolder;
+
+		if( !isset( $tokenValuePairs[ "lang" ] ) )
+		{
+			$builtInTokenValuePairs[ "lang" ] = Language::GetLang();
+		}
+
+		foreach( array_merge( $builtInTokenValuePairs, $tokenValuePairs ) as $token => $value )
+		{
+			$pattern = str_replace( $tokenDelimiter . $token . $tokenDelimiter, $value, $pattern );
+		}
+
+		return( $pattern );
+	}
+}
