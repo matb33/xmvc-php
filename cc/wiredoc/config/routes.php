11020ce4f6ebab4db9f824e419f229f5a2a82663
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-04-01 14:39:58 +0000

    * Removed translate controller, which doesn't work in wiredoc * Moved sitemap file pattern to app folder so it can be modified * Moved components and instances to their own folder structure separate from models/views * Various improvements

--- cc/wiredoc/config/routes.php
+++ cc/wiredoc/config/routes.php
@@ -2,7 +2,6 @@
 
 $routes[ "/^\/robots\.txt$/" ] = "xMVC\\Mod\\CC\\robotstxt";
 $routes[ "/^\/sitemap-([A-Za-z-]+)\.xml$/" ] = "xMVC\\Mod\\CC\\sitemapxml/view/%1";
-$routes[ "/^\/--translate(.*)/" ] = "xMVC\\Mod\\CC\\translate/lang%1";
 
 $lowPriorityRoutes[ "/^\/(.*)$/" ] = "xMVC\\Mod\\CC\\processor/page/%1";
 
