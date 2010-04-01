11d90b1be8e1a321c7e7603c1ae643b19cbaa82f
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-04-01 17:06:55 +0000

    * Part of a change to allow proper casing on filenames

---
+++ mod/favicon/controllers/Favicon.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace xMVC\Mod\Favicon;
+
+class Favicon
+{
+	public function Index()
+	{
+		$this->Ico();
+	}
+
+	public function Ico()
+	{
+		header( "Content-type: image/vnd.microsoft.icon" );
+		echo( file_get_contents( "./app/inc/images/favicon.ico" ) );
+	}
+
+	public function Gif()
+	{
+		header( "Content-type: image/gif" );
+		echo( file_get_contents( "./app/inc/images/favicon.gif" ) );
+	}
+
+	public function Png()
+	{
+		header( "Content-type: image/png" );
+		echo( file_get_contents( "./app/inc/images/favicon.png" ) );
+	}
+}
+
+?>
