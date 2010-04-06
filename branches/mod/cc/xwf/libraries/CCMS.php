b92d944062d9622f4c657adadedf8db9fe6fd03f
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-04-06 13:36:22 +0000

    * Second step in renaming to proper case filenames

---
+++ branches/mod/cc/xwf/libraries/CCMS.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace xMVC\Mod\CC;
+
+use xMVC\Sys\Core;
+use xMVC\Sys\XMLModelDriver;
+use xMVC\Sys\View;
+use xMVC\Sys\Config;
+
+class CCMS
+{
+	public static $container;
+	public static $content;
+
+	public static function SetContainer( $container )
+	{
+		self::$container = $container;
+	}
+
+	public static function SetContent( $content )
+	{
+		self::$content = $content;
+	}
+
+	public static function TransformHTML( $data )
+	{
+		$config = array( "indent" => true, "input-html" => true, "output-xml" => true, "wrap" => false );
+		$tidy = tidy_parse_string( $data, $config, "UTF8" );
+		$tidy->cleanRepair();
+
+		$transformer = new View( Core::namespaceApp . self::$container . ".write" );
+		$transformer->PushModel( new XMLModelDriver( $tidy->value ) );
+
+		return( new XMLModelDriver( $transformer->ProcessAsXML() ) );
+	}
+
+	public static function ImportTransformation( $transformedModel, $contentModel )
+	{
+		$contentModel = new XMLModelDriver( self::GetCCXFile() );
+
+		$nodeToImport = $transformedModel->xPath->query( "/xmvc:root/cc:root" )->item( 0 );
+		$targetNode = $contentModel->xPath->query( "/xmvc:root/cc:root" )->item( 0 );
+		$newNode = $contentModel->importNode( $nodeToImport, true );
+		$targetNode->parentNode->replaceChild( $newNode, $targetNode );
+
+		return( $contentModel );
+	}
+
+	public static function SaveCCX( $contentModel )
+	{
+		$contentModel->normalizeDocument();
+
+		$ccxFile = self::GetCCXFile();
+		$rootNode = $contentModel->xPath->query( "/xmvc:root/cc:root" )->item( 0 );
+
+		$xmlDeclaration = View::GetXMLHead( null, true );
+
+		return( file_put_contents( $ccxFile, $xmlDeclaration . $contentModel->saveXML( $rootNode ) ) );
+	}
+
+	public static function GetCCXFile()
+	{
+		return( Config::$data[ "svnWorkingFolder" ] . self::$container . "/" . self::$content . ".ccx" );
+	}
+}
+
+?>
