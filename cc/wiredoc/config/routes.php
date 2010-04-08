ededede9f2c49d099b08733ccd71b76ea29b154a
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-04-08 15:06:21 +0000

    * Case-sensitive changes to route mapping

--- cc/wiredoc/config/routes.php
+++ cc/wiredoc/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-$routes[ "/^\/robots\.txt$/" ] = "xMVC\\Mod\\CC\\robotstxt";
-$routes[ "/^\/sitemap-([A-Za-z-]+)\.xml$/" ] = "xMVC\\Mod\\CC\\sitemapxml/view/%1";
+$routes[ "/^\/robots\.txt$/" ] = "xMVC\\Mod\\CC\\Robotstxt";
+$routes[ "/^\/sitemap-([A-Za-z-]+)\.xml$/" ] = "xMVC\\Mod\\CC\\Sitemapxml/View/%1";
 
-$lowPriorityRoutes[ "/^\/(.*)$/" ] = "xMVC\\Mod\\CC\\processor/page/%1";
+$lowPriorityRoutes[ "/^\/(.*)$/" ] = "xMVC\\Mod\\CC\\Processor/Page/%1";
 
 ?>
