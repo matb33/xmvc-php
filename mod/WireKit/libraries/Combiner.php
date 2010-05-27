2bc42c0b96617e12ce998f5af87647bcae0b18fb
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-05-27 20:51:47 +0000

    * Added basics of Combiner class

--- mod/WireKit/libraries/Combiner.php
+++ mod/WireKit/libraries/Combiner.php
@@ -4,8 +4,44 @@ namespace xMVC\Mod\WireKit;
 
 class Combiner
 {
-	public static function CombineScripts( $basePath, $scriptNodes )
+	public static function DummyMethod()
 	{
-		return( $basePath . "testing.js" );
+		// This method is here as a hack to have PHP load this class, because calling php:function in XSLT without
+		// first having this class loaded won't work, despite autoloading mechanisms being in place.  This method
+		// is intended to be called in some WireKit core area and essentially do nothing.
+	}
+
+	public static function CombineJavaScripts( $basePath, $scriptNodes )
+	{
+		$files = array();
+
+		foreach( $scriptNodes as $node )
+		{
+			if( $node->hasAttribute( "href" ) )
+			{
+				$files[] = $node->getAttribute( "href" );
+			}
+		}
+
+		$hash = md5( implode( " ", $files ) );
+
+		return( $basePath . "script-" . $hash . ".js" );
+	}
+
+	public static function CombineStylesheetLinks( $basePath, $media, $linkNodes )
+	{
+		$files = array();
+
+		foreach( $linkNodes as $node )
+		{
+			if( $node->hasAttribute( "href" ) )
+			{
+				$files[] = $node->getAttribute( "href" );
+			}
+		}
+
+		$hash = md5( implode( " ", $files ) );
+
+		return( $basePath . "link-" . $media . "-" . $hash . ".css" );
 	}
 }
