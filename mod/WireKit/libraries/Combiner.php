d7bc19049bdd64099b3f243688acc8390b9567ab
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-06-01 19:47:02 +0000

    * Added LOCK_EX on various file_put_contents calls

--- mod/WireKit/libraries/Combiner.php
+++ mod/WireKit/libraries/Combiner.php
@@ -58,7 +58,7 @@ class Combiner
 			}
 
 			$realOutputFile = Config::$data[ "rootPath" ] . "/" . $outputFilename;
-			FileSystem::FilePutContents( $realOutputFile, $fileContents );
+			FileSystem::FilePutContents( $realOutputFile, $fileContents, LOCK_EX );
 		}
 	}
 
