b255f0412e6800ebb6c41a2d7558bfdf9b53440d
pelkhouri@gmail.com <pelkhouri@gmail.com@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-06-03 21:37:18 +0000

    * Added email RegExp * Added email type constraint * Renamed wd:group of a form to wd:option-group -- needs revision * Updated wiredoc-2.0.txt documentation

--- trunk/mod/WireKit/config/config.php
+++ trunk/mod/WireKit/config/config.php
@@ -23,3 +23,4 @@ else
 }
 
 $sourceViewEnabled = !$isProduction;
+$validationEmailRegExp = "^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$";
