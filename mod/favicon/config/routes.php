98e064d40ba66359afe41139294b94cf909f1758
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-06-04 13:05:48 +0000

    * Removed closing php tag ?> as per best practices

--- mod/favicon/config/routes.php
+++ mod/favicon/config/routes.php
@@ -3,5 +3,3 @@
 $routes[ "/favicon\.ico/" ] = "xMVC\\Mod\\Favicon\\Favicon/Ico";
 $routes[ "/favicon\.gif/" ] = "xMVC\\Mod\\Favicon\\Favicon/Gif";
 $routes[ "/favicon\.png/" ] = "xMVC\\Mod\\Favicon\\Favicon/Png";
-
-?>
