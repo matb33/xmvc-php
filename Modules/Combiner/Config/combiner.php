7b441a754b09cfa728445aaa87b4d4c0dd83fb2b
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-06-20 18:03:13 +0000

    * Renamed inc to Public

--- Modules/Combiner/Config/combiner.php
+++ Modules/Combiner/Config/combiner.php
@@ -1,5 +1,5 @@
 <?php
 
-$combinerRewriteAdaptors = array( "|^[/]?(.+)/inc/(.*)|" => "./Modules/$1/inc/$2", "|^[/]?inc/(.*)$|"  => "./Application/inc/$1");
+$combinerRewriteAdaptors = array( "|^[/]?(.+)/inc/(.*)|" => "./Modules/$1/Public/$2", "|^[/]?inc/(.*)$|"  => "./Application/Public/$1");
 $combinerCacheWebFolder = "/inc/cache/";
-$combinerCachePhysicalFolder = "./Application/inc/cache/";
+$combinerCachePhysicalFolder = "./Application/Public/cache/";
