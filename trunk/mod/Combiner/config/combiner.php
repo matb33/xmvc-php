ce48e6f1ae4e3aa1ad8cb1410acc10bd04e50205
matb33 <matb33@669c4392-8544-11de-aa20-d7387d5ed10e>
2010-06-25 15:37:53 +0000

    * Another legacy combiner fix

--- trunk/mod/Combiner/config/combiner.php
+++ trunk/mod/Combiner/config/combiner.php
@@ -1,6 +1,6 @@
 <?php
 
-$combinerRewriteAdaptors = array( "|^(.+)/inc/(.*)|" => "./mod/$1/inc/$2", "|^[/]?inc/(.*)$|" => "./app/inc/$1" );
+// $combinerRewriteAdaptors = array( "|^(.+)/inc/(.*)|" => "./mod/$1/inc/$2", "|^[/]?inc/(.*)$|" => "./app/inc/$1" );	// Define in your app/config
 
 $combinerCacheWebFolder = "/inc/cache/";
 $combinerCachePhysicalFolder = "./app/inc/cache/";
